0c79702a2e0cd31a421063cca3da34f2ba6bfd1f
Peter Oettig <peet1993@gmail.com>
2021-04-05 00:50:20 +0200

    Removed log line.

--- TS3AudioBot/Web/SpotifyApi.cs
+++ TS3AudioBot/Web/SpotifyApi.cs
@@ -232,8 +232,6 @@ namespace TS3AudioBot.Web {
 					break;
 				}
 
-				Log.Warn($"Spotify request is fucked: {result.Error.Item1} {result.Error.Item2}");
-
 				if (result.Error.Item1 != TimeSpan.Zero) {
 					// Rate limit was already honored three times, don't do that again.
 					if (ratelimitHonored >= 3) {
